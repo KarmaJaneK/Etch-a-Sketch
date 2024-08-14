5dd78fec72bdf785dda5707727523fc3f69f965b
KarmaJaneK <97600912+KarmaJaneK@users.noreply.github.com>
2024-08-14 11:32:47 +0700

    Added eventListeners for hover effect

--- etchASketch.js
+++ etchASketch.js
@@ -6,13 +6,24 @@ function createGrid (rows, columns){
     for (let i = 0; i < rows; i ++){
     const gridSquareRow = document.createElement('div');
     gridSquareRow.classList.add('row');
-    gridSquareRow.textContent = "row";
     container.appendChild(gridSquareRow);
-    }
+    
 for (let j = 0; j  < columns; j++){
     const gridSquareColumn = document.createElement('div');
     gridSquareColumn.classList.add('column');
-    gridSquareColumn.textContent = "Column";
-    container.appendChild(gridSquareColumn);}
-}
-createGrid(3,4);
+    gridSquareColumn.textContent = "Colum";
+    gridSquareRow.appendChild(gridSquareColumn);}
+}}
+
+
+
+createGrid(100,100)
+
+document.querySelectorAll('.row, .column').forEach(element =>{
+    element.addEventListener('mouseenter', () => {
+        element.style.backgroundColor = 'black';
+    });
+    element.addEventListener('mouseleave', () => {
+        element.style.backgroundColor = 'black';
+    });
+});
